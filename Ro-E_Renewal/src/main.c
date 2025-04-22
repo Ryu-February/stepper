cf735054b58cfbe0935ffe51faee7bf4054a192c
rcy <rcy@roborobo.co.kr>
2025-04-22 17:53:06 +0900

    chore: first init

---
+++ Ro-E_Renewal/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * Ro-E_Renewal.c
+ *
+ * Created: 2025-04-22 오후 5:49:56
+ * Author : RCY
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
